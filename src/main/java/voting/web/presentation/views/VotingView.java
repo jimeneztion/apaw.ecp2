58ecdd93d233455471809cbdaea59cea2c0adf3e
Sergio Gil <sergio.giljimenez@alumnos.upm.es>
2015-10-22 14:34:26 +0200

    Vista vote

--- src/main/java/voting/web/presentation/views/VotingView.java
+++ src/main/java/voting/web/presentation/views/VotingView.java
@@ -8,13 +8,14 @@ public class VotingView implements View {
     @Override
     public void show(Model model) {
         StringBuilder sb = new StringBuilder();
-        sb.append("Voting Page");
+        sb.append("Voting Page\n");
+        sb.append("Temas: [");
         List<String> listVotes = (List<String>) model.get("votes");
         for (int i = 0; i < listVotes.size(); i++) {
             sb.append("ThemeTransfer " + listVotes.get(i));
         }
-        sb.append(" ] \n");
-        sb.toString();
+        sb.append(" ]");
+        System.out.println(sb.toString());
     }
 
     
